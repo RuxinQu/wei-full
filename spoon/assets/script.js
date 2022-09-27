1e04a0182c2802cbdf1a5d7a89492bc001274262
Ruxin Qu <rosheenqu@gmail.com>
2022-09-27 00:35:51 -0700

    add styling to the searchByIngredient page

--- spoon/assets/script.js
+++ spoon/assets/script.js
@@ -2,12 +2,11 @@ $(document).ready(() => {
     const apiKey = `6e8b2acac15a42fc99b75f6f8e92662f`;
     const baseUrl = `https://api.spoonacular.com`
 
-
     const findByIngredient = async () => {
         const ingredient = $('#ingredient').val();
-        $('#ingredient').val('');
+
         const endPoint = `/recipes/findByIngredients`;
-        const requestParams = `?apiKey=${apiKey}&ingredients=${ingredient}&number=5`;
+        const requestParams = `?apiKey=${apiKey}&ingredients=${ingredient}&number=6`;
         const urlToFetch = `${baseUrl}${endPoint}${requestParams}`;
         try {
             const response = await fetch(urlToFetch);
@@ -20,52 +19,65 @@ $(document).ready(() => {
             console.log(error.message);
         }
     }
-    const renderTitle = (title) => {
-        const titleEl = $('<h3>');
-        titleEl.text(title);
-        $('.output').append(titleEl);
-    }
-    const renderImg = (url) => {
-        const img = $('<img>');
-        img.attr({
-            src: url,
-            alt: 'recipe image'
-        })
-        $('.output').append(img)
+
+    const createCards = () => {
+        const left = $('<i class="before fa fa-angle-left" style="font-size:24px"></i>')
+        $('.output').append(left)
+        for (let x = 0; x < 3; x++) {
+
+            const card = $("<div class='card'>")
+            card.attr('id', `card${x + 1}`)
+            $('.output').append(card)
+        }
+        const right = $('<i class="after fa fa-angle-right" style="font-size:24px"></i>')
+        $('.output').append(right)
     }
 
-    const renderMissedIngredients = (missedArr) => {
-        const missIngreList = $('<ul>');
-        missIngreList.text(`other ingredient:`);
-        for (let x = 0; x < missedArr.length; x++) {
-            const missIngreItem = $('<li>')
-            missIngreItem.text(missedArr[x].name);
-            missIngreList.append(missIngreItem)
-            $('.output').append(missIngreList);
+    const renderIngredientResult = (result, index) => {
+        for (let x = 0; x < 3; x++) {
+            const titleEl = $(`<h3>`);
+            titleEl.text(result[index].title).css({ 'fontSize': '1.5rem', 'text-align': 'center' })
+            const img = $('<img>');
+            img.attr({
+                src: result[index].image,
+                alt: 'recipe image',
+                style: 'width: 200px; height: 200px'
+            })
+            const missIngreList = $('<ul>');
+            missIngreList.text(`other ingredient:`).css('fontSize', '1.2rem');
+            const missedArr = result[index].missedIngredients;
+            for (let x = 0; x < missedArr.length; x++) {
+                const missIngreItem = $('<li>')
+                missIngreItem.text(missedArr[x].name);
+                missIngreList.append(missIngreItem)
+            }
+            $(`#card${x + 1}`).append(titleEl).append(img).append(missIngreList);
+            index += 1;
         }
     }
-
-
-    const renderIngredientResult = (result) => {
-        for (let x = 0; x < result.length; x++) {
-            renderTitle(result[x].title);
-            renderImg(result[x].image);
-            renderMissedIngredients(result[x].missedIngredients)
-            $('.output').append('<hr>')
-
-        }
-    }
-
-    const search = async () => {
+    const search = async (index) => {
         const result = await findByIngredient();
-        renderIngredientResult(result);
+        console.log(result);
+        if (result.length == 0) {
+           return ''
+        } else {
+            createCards();
+            renderIngredientResult(result, index);
+        }
     }
 
-    $('.btn').on('click', (event) => {
+    $('.search').on('click', (event) => {
         event.preventDefault();
         $('.output').text('')
-        search();
+        search(0);
     })
 
-
+    $('.output').on('click', '.before', () => {
+        $('.output').text('');
+        search(0);
+    })
+    $('.output').on('click', '.after', () => {
+        $('.output').text('');
+        search(3);
+    })
 })
