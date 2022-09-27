d936f58f7b74d7e04c163100e3cd3950493f0a08
Haleigh Rzonca <hgielah@gmail.com>
2022-09-27 10:12:35 -0700

    Saving before i start today. Added some js but not much. trying to get a functional api.

--- rest/assets/rest.js
+++ rest/assets/rest.js
@@ -3,7 +3,8 @@ var googlePlacesApiKey = "AIzaSyDszuLl_M28FKlLy8cXvTphuTBYkhl4Yks";
 var baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 }
 
-function showResaturant() {
-    var queryUrl = baseUrl + "type" + "photo" + "address" + ""  + googlePlacesApiKey
+const findPlace = async () => {
+const place = $('rest').val();
+$('#rest').val('');
 
 }
