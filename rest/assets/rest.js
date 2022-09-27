fca7d5ab863a159dfd40babc1c5b2c43847f7c7b
Haleigh Rzonca <hgielah@gmail.com>
2022-09-26 20:35:13 -0700

    added html and js for restaurant side

---
+++ rest/assets/rest.js
@@ -0,0 +1,9 @@
+$(document).ready() => {
+var googlePlacesApiKey = "AIzaSyDszuLl_M28FKlLy8cXvTphuTBYkhl4Yks";
+var baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+}
+
+function showResaturant() {
+    var queryUrl = baseUrl + "type" + "photo" + "address" + ""  + googlePlacesApiKey
+
+}
