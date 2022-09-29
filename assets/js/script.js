32087e37ace2a7cea721949c9d48bef548bc530d
Haleigh Rzonca <hgielah@gmail.com>
2022-09-28 19:12:39 -0700

    got the weather api to show in the console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,47 +11,38 @@ document.getElementById('out').addEventListener('click', () => {
 })
 
 // show the weather for that night 
-const newName = document.querySelector('.city');
-const fetchButton = document.querySelector('.search')
+let city = "";
 let weatherApiKey = '2c73f79417295159a86a0353b1677f02';
-let baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + newName.value + '&units=imperical&appid=' + weatherApiKey;
 
-const showWeather = async (city) => {
+
+function showWeather (city) {
+    console.log(city)
     const urlToFetch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperical&appid=${weatherApiKey}`;
-    try {
-        const response = await fetch(urlToFetch);
-        if (response.ok) {
-            const forecastJsonResponse = await response.json();
-            const list = forecastJsonResponse;
-            return list;
-        }
-    } catch (err) {
-        console.log(err);
-    }
-}
+    console.log(urlToFetch)
+    fetch(urlToFetch) 
+.then(response => {
+    console.log(response);
+    return response.json();
 
+}) 
+.then( data => {
+    console.log(data);
+})
+    }
 
 const displayForecast = (list) => {
     document.getElementsByClassName("temp")[0].innerHTML = "Temp: " + data.main.temp + "°F";
-    document.getElementsByClassName("condition")[0].innerHTML = "Condition: " + list.weather[0].discription;
+    document.getElementsByClassName("condition")[0].innerHTML = "Condition: " + data.weather[0].discription;
     document.getElementsByClassName("icon")[0].innerHTML = `http://openweathermap.org/img/wn/${list.weather[0].icon}@2x.png`
 }
-
-
-const searchForcast=async()=>{
-    const list = await showWeather(city);
-    displayForecast(list);
-}
-
 
 $(".search").on('click',(event)=>{
     event.preventDefault();
     city = $('.city').val();
-    showWeather();
-    searchForcast();
+    console.log(city);
+    showWeather(city);
 })
 
-fetchButton.addEventListener("click", showWeather);
 
 var currentDay = moment();
 $(".day").text(date.format("dddd"))
